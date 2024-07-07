9075fbcb0b946651c8eddcca68ebdea26ec0da86
leothehero <leothehero@hotmail.co.uk>
2024-07-07 15:57:15 +0200

    Spelling Change

--- PSMMain.py
+++ PSMMain.py
@@ -57,7 +57,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.FleetTracker = FleetTracker(self.PDM)
         self.FleetTracker.show()
 
-        print("PSM: MainWindow initialization complete")
+        print("PSM: Main Window initialization complete")
     
     def getColouredIcon(self,resourceLocator,color='black'): 
         pixmap = QPixmap(resourceLocator)
